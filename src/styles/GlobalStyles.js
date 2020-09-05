78df17b1d92eeb1d7e8f00ab78b4d9fd9372ed55
Gabriel <knuckles4x@gmail.com>
2020-09-04 23:29:57 -0300

    initial setup

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: var(--white);
+  }
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+    height: 100%;
+    width: 100%;
+  }
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: --apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  :root {
+    --primary: #000;
+    --yellow: #FFCB05;
+    --blue: #3D7DCA;
+    --navy-blue: #003A70;
+    --red: #FF0000;
+    --boston-red: #CC0000;
+  }
+`;
